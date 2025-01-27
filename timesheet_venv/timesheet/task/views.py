9fa6160b6569d0c38fefad15cea859fc8710bb2f
Shreeya Adhikari <144326405+shreeyaadhikari@users.noreply.github.com>
2025-01-27 13:19:18 +0545

    Update views.py
    added view to create table and create filter to sort records by user

--- timesheet_venv/timesheet/task/views.py
+++ timesheet_venv/timesheet/task/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
+from .models import ticket
 
 # Create your views here.
 
 def task(request):
-    return render(request, 'task.html')
+    users = User.objects.all() 
+    tasks = ticket.objects.all()  
+    return render(request, 'task.html', {'users': users, 'tasks': tasks})
+    
+
+
+def filterTaskByUser(request, id):
+    users = User.objects.all()
+    user = get_object_or_404(User, pk=id) 
+    tasks = ticket.objects.filter(assigned_to=user)  
+    return render(request, 'task.html', {'users': users, 'tasks': tasks})
