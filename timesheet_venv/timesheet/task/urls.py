cecef862fa4b8b7a6c78a1fd22fe399ded23633e
tilasmi13 <tilasmi.prajapati@gmail.com>
2025-01-29 11:21:44 +0545

    add task views

--- timesheet_venv/timesheet/task/urls.py
+++ timesheet_venv/timesheet/task/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     #task
     path('task/',views.task, name='task'),
     path('taskdetails/',views.taskdetails, name='taskdetails'),
+    path('add/',views.add_task, name='add_task'),
 ]
--- timesheet_venv/timesheet/task/views.py
+++ timesheet_venv/timesheet/task/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import ticket
 
 # Create your views here.
 
@@ -8,3 +9,27 @@ def task(request):
 def taskdetails(request):
     return render(request, 'taskdetails.html')
 
+def add_task(request):
+    if request.method == 'POST':
+        ticket_name = request.POST.get('title')
+        customer = request.POST.get('customer')
+        short_description = request.POST.get('short_description')
+        ticket_type = request.POST.get('ticket_type')
+        date_opened = request.POST.get('date_opened')
+        assigned_user = request.POST.get('assigned_user')
+        priority = request.POST.get('priority')
+        comments = request.POST.get('comments')
+
+        # Create and save the new task
+        ticket = ticket(
+            ticket_name=ticket_name,
+            customer=customer,
+            short_description=short_description,
+            task_type=task_type,
+            assigned_user=assigned_user,
+            priority=priority,
+            comments=comments,
+        )
+        ticket.save()
+        return redirect('taskdetails', task_id=task.id)
+    return render(request, 'task.html')
