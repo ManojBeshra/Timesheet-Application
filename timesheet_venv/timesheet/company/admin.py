7cabf396ee4cdb68804ab66b3b94c3ec5aa82635
ManojBeshra <106796414+ManojBeshra@users.noreply.github.com>
2025-01-29 15:17:35 +0545

    Delete old directory

--- timesheet_venv/timesheet/company/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import company
-# Register your models here.
-
-class companyAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name']
-admin.site.register(company, companyAdmin)
