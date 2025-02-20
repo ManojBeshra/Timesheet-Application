5374be1c6d6522eab450f8a4f5edcc227b148ebf
Shreeya Adhikari <adh.shreeya@gmail.com>
2025-02-20 10:47:17 +0545

    worklog

--- timesheet_venv/timesheet/worklog/admin.py
+++ timesheet_venv/timesheet/worklog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import worklog
 
 # Register your models here.
+# Register your models here.
+class worklog_admin(admin.ModelAdmin):
+    list_display = ['user', 'date', 'ticket_type', 'detail', 'project_support', 'ticket', 'hours', 'billable', 'note', 'priority', 'category', 'week']
+
+admin.site.register(worklog, worklog_admin)
--- timesheet_venv/timesheet/worklog/models.py
+++ timesheet_venv/timesheet/worklog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from task .models import ticket, project, priority_type, ticket_type
 
-# Create your models here.
+
+class worklog(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    date = models.DateTimeField(null=True)
+    ticket_type = models.ForeignKey(ticket_type, on_delete=models.CASCADE, null=True, related_name="worklog_ticket_type")
+    detail = models.CharField(max_length=200, null=True)
+    project_support = models.ForeignKey(project, on_delete=models.CASCADE, null=True, related_name="worklog_project_support")
+    ticket = models.ForeignKey(ticket, on_delete=models.CASCADE, null=True, related_name="worklog_ticket")
+    hours = models.IntegerField(null=True)
+    billable = models.BooleanField(null=True)
+    note = models.CharField(max_length=200, default="", null=True)
+    priority = models.ForeignKey(priority_type, on_delete=models.CASCADE, null = True)
+    category = models.CharField(max_length=100, null=True)
+    week = models.CharField(max_length=100, null=False)
