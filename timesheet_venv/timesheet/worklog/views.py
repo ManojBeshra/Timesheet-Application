b41ebfaa616bf298b03e5a05038121b77e6111b1
Shreeya Adhikari <adh.shreeya@gmail.com>
2025-02-28 10:57:49 +0545

    Added test html

--- timesheet_venv/timesheet/worklog/views.py
+++ timesheet_venv/timesheet/worklog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from task.models import priority_type, ticket
-
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
 
 
 # Create your views here.
@@ -12,3 +14,36 @@ def worklog(request):
     users = User.objects.all()
     tickets = ticket.objects.all()
     return render(request, 'worklog.html', {'priority': priority, 'tickets': tickets, 'users': users})
+
+
+
+@login_required
+@csrf_exempt
+def request_review(request):
+    if request.method == 'POST':
+        try:
+            
+            date_requested = request.POST.get('date')
+            assigned_users = request.POST.getlist('assigneduserlist[]') # Get multiple users
+            operational_notes = request.POST.get('shortdescriptionarea')
+           
+
+            # Create new request
+            new_request = request(
+                date_requested=date_requested,
+                operational_notes=operational_notes,
+            )
+
+            new_request.save()
+
+
+            # Assign multiple users
+            assigned_user_objs = User.objects.filter(id__in=assigned_users)
+            new_request.assigned_to.set(assigned_user_objs)  # Use `.set()` for ManyToManyField
+
+            return JsonResponse({"success": True, "message": "Request added successfully!"})
+        except Exception as e:
+            print(f"Error: {e}")  # Debugging
+            return JsonResponse({"success": False, "message": str(e)}, status=400)
+
+    return JsonResponse({"success": False, "message": "Invalid request"}, status=400)
