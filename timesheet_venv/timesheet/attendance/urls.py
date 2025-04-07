9e317db8345f42bb86f4a038aafd37ba3b84544d
Shreeya Adhikari <144326405+shreeyaadhikari@users.noreply.github.com>
2025-04-07 16:22:24 +0545

    updated urls.py of attendance

--- timesheet_venv/timesheet/attendance/urls.py
+++ timesheet_venv/timesheet/attendance/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('leavedetails/',views.leavedetails_view, name='leavedetails'),
     # Add this path for message update
     path('save-note/<int:record_id>/', views.save_note_view, name='save_note'),
+    path("delete-attendance/", views.delete_attendance, name="delete_attendance"),
     
 ]
