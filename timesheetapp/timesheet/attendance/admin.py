f63a8ba779f6983b35e97650761558604009c837
Shreeya Adhikari <144326405+shreeyaadhikari@users.noreply.github.com>
2025-07-08 16:41:12 +0545

    Update admin.py
    Added userleavedays in admin.py

--- timesheetapp/timesheet/attendance/admin.py
+++ timesheetapp/timesheet/attendance/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from attendance.models import Attendance, AttendanceDetail, LeaveType, LeaveDetails, Approval
+from attendance.models import Attendance, AttendanceDetail, LeaveType, LeaveDetails, Approval, userLeaveDays
 
 # Register your models here.
 class AttendanceAdmin(admin.ModelAdmin):
@@ -15,10 +15,14 @@ class ApprovalAdmin(admin.ModelAdmin):
     list_display = ('id','name')
 
 class LeaveDetailsAdmin(admin.ModelAdmin):
-    list_display = ('requested_date', 'type', 'leave_from', 'leave_to', 'approval', 'user')
+    list_display = ('requested_date', 'type', 'leave_from', 'leave_to', 'approval', 'user', 'approvedby')
+
+class userLeaveDaysAdmin(admin.ModelAdmin):
+    list_display = ('user', 'type', 'leaveTaken', 'availableDays')
 
 admin.site.register(Attendance, AttendanceAdmin)
 admin.site.register(AttendanceDetail, AttendanceDetailAdmin)
 admin.site.register(LeaveType, LeaveTypeAdmin)
 admin.site.register(Approval, ApprovalAdmin)
 admin.site.register(LeaveDetails, LeaveDetailsAdmin)
+admin.site.register(userLeaveDays, userLeaveDaysAdmin)
